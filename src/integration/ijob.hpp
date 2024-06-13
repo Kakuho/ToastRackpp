8b7f9805d6a5272c7121f101e9a576c9759ff627
ka <kaku.h9@gmail.com>
2024-06-13 17:34:18 +0100

    Made integration procedure wrappers

---
+++ src/integration/ijob.hpp
@@ -0,0 +1,65 @@
+#ifndef IJOB_HPP
+#define IJOB_HPP
+
+#include <string>
+#include <iostream>
+
+namespace integ{
+
+  template<typename T>
+  concept Callable = requires(T a){
+    a();
+  };
+
+  template<typename T>
+    requires Callable<T>
+  class IJob{
+    private:
+      static constexpr std::size_t width = 100;
+
+    public:
+      // the only constructors we allow
+      IJob(std::string&& name, T proc): m_name{name}, m_proc{proc}{
+      }
+
+      IJob(T proc): m_name{"unnamed procedure"}, m_proc{proc}{    // should not really allow this, but yk if lazy is ok
+      }
+
+      // do not allow copying / moving to provide function-like semantics
+      IJob() = delete;
+      IJob(const IJob& src) = delete;
+      IJob(const IJob&& src) = delete;
+      IJob& operator=(const IJob& src) = delete;
+      IJob& operator=(const IJob&& src) = delete;
+
+      void PrintHeader(std::size_t width){
+        for(int i = 0; i < width; i++){
+          std::cout << '-';
+        }
+
+        std::cout << "\nIntegration Prodcedure :: " << m_name << "\nStarting...\n\n";
+      };
+
+      void PrintFooter(std::size_t width){
+        
+        std::cout << "\nEnding...\nIntegration Prodcedure :: " << m_name << '\n';
+        for(int i = 0; i < width; i++){
+          std::cout << '-';
+        }
+        std::cout << '\n';
+      };
+
+      void operator()(){
+        PrintHeader(IJob::width);
+        m_proc();
+        PrintFooter(IJob::width);
+      }
+
+    private:
+      T m_proc;
+      std::string m_name;
+  };
+
+}; // namespace integ
+
+#endif
--- src/integration/loader.hpp
+++ src/integration/loader.hpp
@@ -1,43 +1,40 @@
 #include <iostream>
 #include <bitset>
-#include "./../loader/taploader.hpp"
-#include "./../loader/z80loader.hpp"
-#include "./../48k/48kmemory.hpp"
+#include "ijob.hpp"
+#include "loader/taploader.hpp"
+#include "loader/z80loader.hpp"
+#include "48k/memory48k.hpp"
 
 namespace integ{
-  inline void testTapLoader(){
-    using namespace trpp;
-    TapLoader tl{"../assets/tap/hero.tap"};
-    //tl.PrintRawBytes();
-    std::cout << "ayo!" << '\n';
-    //tl.PrintRawBytes(0x13 + 2);
-    tl.PrintChunks();
-  }
 
-  inline void testZ80Loader(){
-    using namespace trpp;
-    Z80Loader zl{"../assets/z80/CALIPSE.Z80"}; // note CALIPSE.Z80 is not compressed
-    zl.PrintRawBytes(35);
-    zl.PrintHeader();
-    std::cout << std::bitset<8>(zl.ParseRefreshRegister()).to_ulong() << '\n';
-    std::cout << "ayo!" << '\n';
-  }
+  inline IJob testTapLoader{"test .tap loader", 
+    [](){
+      using namespace trpp;
+      TapLoader tl{"../assets/tap/hero.tap"};
+      //tl.PrintRawBytes();
+      std::cout << "ayo!" << '\n';
+      //tl.PrintRawBytes(0x13 + 2);
+      tl.PrintChunks();
+    }
+  };
 
-  inline void testZ80LoaderMachineModel(){
-    std::cout << "--------------------------------\nIntegration Prodcedure Start\n\n";
-    using namespace trpp;
-    Z80Loader zl{"../assets/z80/CALIPSE.Z80"}; // note CALIPSE.Z80 is not compressed
-    zl.PrintRawBytes(35);
-    zl.PrintSpectrumModel();
-    std::cout << "\nIntegration Prodcedure End\n--------------------------------\n";
-  }
+  inline IJob testZ80Loader{"test .z80 loader", 
+    [](){
+      using namespace trpp;
+      Z80Loader zl{"../assets/z80/CALIPSE.Z80"}; // note CALIPSE.Z80 is not compressed
+      zl.PrintRawBytes(35);
+      zl.PrintHeader();
+      std::cout << std::bitset<8>(zl.ParseRefreshRegister()).to_ulong() << '\n';
+      std::cout << "ayo!" << '\n';
+    }
+  };
 
-  inline void testZ80LoaderMachineModelTetris(){
-    std::cout << "--------------------------------\nIntegration Prodcedure Start\n\n";
-    using namespace trpp;
-    Z80Loader zl{"../assets/z80/TETRIS.Z80"}; // note CALIPSE.Z80 is not compressed
-    zl.PrintRawBytes(35);
-    zl.PrintSpectrumModel();
-    std::cout << "\nIntegration Prodcedure End\n--------------------------------\n";
-  }
+  inline IJob testLoaderTetris{"test tetris loader", 
+    [](){
+      using namespace trpp;
+      Z80Loader zl{"../assets/z80/TETRIS.Z80"}; // note CALIPSE.Z80 is not compressed
+      zl.PrintRawBytes(35);
+      zl.PrintSpectrumModel();
+    }
+  };
 }
